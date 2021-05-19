b14c19b224f696ac4ebf4ad168b5e2d38a26a75f
Pc.unique <zahraeng369@gmail.com>
2021-05-19 17:09:10 +0430

    product-backend-api

--- server.js
+++ server.js
@@ -23,18 +23,18 @@ const Product = mongoose.model("products",new mongoose.Schema({
 }))
 
 
-app.get("api/products",async(req,res)=>{
+app.get("/api/products",async(req,res)=>{
     const products = await Product.find({});
     res.send(products);
 });
 
-app.post("api/products",async(req,res)=>{
+app.post("/api/products",async(req,res)=>{
     const newProduct = new Product(req.body);
     const savedProduct = await newProduct.save();
     res.send(savedProduct);
 });
 
-app.delete("api/products/:id",async(req,res)=>{
+app.delete("/api/products/:id",async(req,res)=>{
     const deletedProduct = await Product.findByIdAndDelete(req.params.id);
     res.send(deletedProduct);
 })
